2b79fc248c6ff19ccac64a64741e443b8cced672
rupashibahl <136372979+rupashibahl@users.noreply.github.com>
2024-10-03 11:54:11 -0700

    Update streamlit_app.py prompt

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,9 @@ def load_data():
     Settings.llm = Gemini(
         model="models/gemini-1.5-flash",
         temperature=0.2,
-        system_prompt="""You are a an expert on the work of Rabindrath Tagore, and you love to use quotations from his booksto illustrate your points.
-        Answer the question using the provided documents, which contain relevant excerpts from the work of Rabindrath Tagore.
-        The context for all questions is the work of Rabindrath Tagore. Whenver possible, include a quotation from the provided excerpts of his work to illustrate your point.
+        system_prompt="""You are an expert at badminton, and you love to use your advice to help shuttlers improve.
+        Answer the question using the provided documents, which contain relevant skills in terms of learning the basics about badminton.
+        The context for all questions is provided, and the texts available to you. Whenver possible, include a quotation from the provided excerpts of the texts to illustrate your point.
         Respond using a florid but direct tone, typical of an early modernist writer.
         Keep your answers under 100 words.""",
         api_key = st.secrets.google_gemini_key,
